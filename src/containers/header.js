a03ae14a0a209603eb854888682a54ae75571303
Paras Solanki <parassolanki151@gmail.com>
2021-06-01 16:08:03 +0530

    added header container

---
+++ src/containers/header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Header, Button } from "../components";
+import * as ROUTES from "../constants/routes";
+
+export default function HeaderContainer({ children, ...restProps }) {
+  return (
+    <Header src="home-bg" {...restProps}>
+      <Header.Nav>
+        <Header.Logo to={ROUTES.HOME}>Movie App</Header.Logo>
+        <div>
+          <Button
+            href={ROUTES.SIGN_IN}
+            bg="primary"
+            color="white"
+            padding=".2rem .9rem"
+          >
+            Sign in
+          </Button>
+          <Button
+            href={ROUTES.SIGN_UP}
+            bg="white"
+            color="primary"
+            padding=".2rem .9rem"
+          >
+            Sign up
+          </Button>
+        </div>
+      </Header.Nav>
+      {children}
+    </Header>
+  );
+}
