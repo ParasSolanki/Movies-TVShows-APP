4f04569a874ad6d706f6031ed6f9eb31d05af553
Paras Solanki <parassolanki151@gmail.com>
2021-06-01 14:51:07 +0530

    added pages file

---
+++ src/pages/browse.js
@@ -0,0 +1,13 @@
+import useContent from "../hooks/use-content";
+import { movieURLs, tvShowsURLs } from "../fixtures/data";
+import BrowseContainer from "../containers/Browse";
+import selectionFilter from "../utils/selection-filter";
+
+export default function Browse() {
+  const { tvShows } = useContent(tvShowsURLs, "tvShows");
+  const { movies } = useContent(movieURLs, "movies");
+
+  const slides = selectionFilter({ movies, tvShows });
+
+  return <BrowseContainer slides={slides} />;
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,11 @@
-import useContent from "../hooks/use-content";
-import { movieURLs, tvShowsURLs } from "../fixtures/data";
-import BrowseContainer from "../containers/Browse";
-import selectionFilter from "../utils/selection-filter";
+import React from "react";
+import HeaderContainer from "../containers/header";
 
 export default function Home() {
-  const { tvShows } = useContent(tvShowsURLs, "tvShows");
-  const { movies } = useContent(movieURLs, "movies");
-
-  const slides = selectionFilter({ movies, tvShows });
-
-  return <BrowseContainer slides={slides} />;
+  return (
+    <div>
+      <HeaderContainer />
+      Hello from Home
+    </div>
+  );
 }
