e567d2227b1117395dc98ee2784aba0d984f1a87
Paras Solanki <parassolanki151@gmail.com>
2021-06-09 21:16:40 +0530

    added more category in data for more movies & tvshows in browse page

--- src/fixtures/data.js
+++ src/fixtures/data.js
@@ -10,11 +10,13 @@ export const movieURLs = [
     category: "Top Rated",
   },
   {
-    // https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate
+    url: `https://api.themoviedb.org/3/trending/movie/day?api_key=${api_key}&language=en-US`,
+    category: "Trending",
+  },
+  {
     url: `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US&with_genres=878`,
     category: "Sci-Fi",
   },
-
   {
     url: `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US&with_genres=28`,
     category: "Action",
@@ -22,6 +24,26 @@ export const movieURLs = [
   {
     url: `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US&with_genres=27`,
     category: "Horror",
+  },
+  {
+    url: `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US&with_genres=99`,
+    category: "Documentary",
+  },
+  {
+    url: `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US&with_genres=80`,
+    category: "Crime",
+  },
+  {
+    url: `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US&with_genres=53`,
+    category: "Thriller",
+  },
+  {
+    url: `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US&with_genres=9648`,
+    category: "Mystery",
+  },
+  {
+    url: `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US&with_genres=16`,
+    category: "Animation",
   },
 ];
 
@@ -31,15 +53,17 @@ export const tvShowsURLs = [
     category: "Top Rated",
   },
   {
+    url: `https://api.themoviedb.org/3/trending/tv/day?api_key=${api_key}&language=en-US`,
+    category: "Trending",
+  },
+  {
     url: `https://api.themoviedb.org/3/tv/popular?api_key=${api_key}&language=en-US`,
     category: "Popular",
   },
   {
-    // https://api.themoviedb.org/3/discover/tv?api_key=${api_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate
     url: `https://api.themoviedb.org/3/discover/tv?api_key=${api_key}&language=en-US&with_genres=878`,
     category: "Sci-Fi & Fantasy",
   },
-
   {
     url: `https://api.themoviedb.org/3/discover/tv?api_key=${api_key}&language=en-US&with_genres=10759`,
     category: "Action & Adventure",
@@ -47,5 +71,21 @@ export const tvShowsURLs = [
   {
     url: `https://api.themoviedb.org/3/discover/tv?api_key=${api_key}&language=en-US&with_genres=99`,
     category: "Documentary",
+  },
+  {
+    url: `https://api.themoviedb.org/3/discover/tv?api_key=${api_key}&language=en-US&with_genres=16`,
+    category: "Animation",
+  },
+  {
+    url: `https://api.themoviedb.org/3/discover/tv?api_key=${api_key}&language=en-US&with_genres=80`,
+    category: "Crime",
+  },
+  {
+    url: `https://api.themoviedb.org/3/discover/tv?api_key=${api_key}&language=en-US&with_genres=9648`,
+    category: "Mystery",
+  },
+  {
+    url: `https://api.themoviedb.org/3/discover/tv?api_key=${api_key}&language=en-US&with_genres=37`,
+    category: "Western",
   },
 ];
--- src/utils/selection-filter.js
+++ src/utils/selection-filter.js
@@ -2,6 +2,11 @@ export default function selectionFilter({ movies, tvShows } = []) {
   return {
     movies: [
       {
+        title: "Trending Movies",
+        data: movies.filter((item) => item.category === "Trending"),
+        category: "movies",
+      },
+      {
         title: "Top Rated Movies",
         data: movies.filter((item) => item.category === "Top Rated"),
         category: "movies",
@@ -12,22 +17,47 @@ export default function selectionFilter({ movies, tvShows } = []) {
         category: "movies",
       },
       {
-        title: "Sci-Fi Movies",
+        title: "Sci-Fi",
         data: movies.filter((item) => item.category === "Sci-Fi"),
         category: "movies",
       },
       {
-        title: "Action Movies",
+        title: "Action",
         data: movies.filter((item) => item.category === "Action"),
         category: "movies",
       },
       {
-        title: "Horror Movies",
-        data: movies.filter((item) => item.category === "Horror"),
+        title: "Documentary",
+        data: movies.filter((item) => item.category === "Documentary"),
+        category: "movies",
+      },
+      {
+        title: "Crime",
+        data: movies.filter((item) => item.category === "Crime"),
+        category: "movies",
+      },
+      {
+        title: "Thriller",
+        data: movies.filter((item) => item.category === "Thriller"),
+        category: "movies",
+      },
+      {
+        title: "Mystery",
+        data: movies.filter((item) => item.category === "Mystery"),
+        category: "movies",
+      },
+      {
+        title: "Animation Movies",
+        data: movies.filter((item) => item.category === "Animation"),
         category: "movies",
       },
     ],
     tvShows: [
+      {
+        title: "Trending TV Shows",
+        data: tvShows.filter((item) => item.category === "Trending"),
+        category: "tv",
+      },
       {
         title: "Top Rated TV Shows",
         data: tvShows.filter((item) => item.category === "Top Rated"),
@@ -39,18 +69,38 @@ export default function selectionFilter({ movies, tvShows } = []) {
         category: "tv",
       },
       {
-        title: "Sci-Fi & Fantasy TV Shows",
+        title: "Sci-Fi & Fantasy",
         data: tvShows.filter((item) => item.category === "Sci-Fi & Fantasy"),
         category: "tv",
       },
       {
-        title: "Action & Adventure TV Shows",
+        title: "Action & Adventure",
         data: tvShows.filter((item) => item.category === "Action & Adventure"),
         category: "tv",
       },
       {
-        title: "Documentary TV Shows",
+        title: "Documentary",
         data: tvShows.filter((item) => item.category === "Documentary"),
+        category: "tv",
+      },
+      {
+        title: "Animation",
+        data: tvShows.filter((item) => item.category === "Animation"),
+        category: "tv",
+      },
+      {
+        title: "Crime",
+        data: tvShows.filter((item) => item.category === "Crime"),
+        category: "tv",
+      },
+      {
+        title: "Mystery",
+        data: tvShows.filter((item) => item.category === "Mystery"),
+        category: "tv",
+      },
+      {
+        title: "Western",
+        data: tvShows.filter((item) => item.category === "Western"),
         category: "tv",
       },
     ],
